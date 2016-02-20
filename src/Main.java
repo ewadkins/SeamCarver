16456daa1e4bc4e9386f7ac9456ddf9f61ccd053
ewadkins <ewadkins83@gmail.com>
2016-02-19 22:19:12 -0500

    Added sobel filtering

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,13 @@ import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,7 +34,25 @@ import javax.swing.border.EtchedBorder;
 
 public class Main {
 	
-	private static final int MAX_DIMENSION = 600;
+	private static final Dimension MAX_DIMENSION = new Dimension(1000, 600);
+	private static final Dimension MIN_DIMENSION = new Dimension(40, 40);
+	private static final double INITIAL_SIZE_RATIO = 0.75;
+	
+	private static BufferedImage bufferedImage = null;
+
+	private static int originalWidth;
+	private static int originalHeight;
+	private static int scaledWidth;
+	private static int scaledHeight;
+
+	private static int lastWidth;
+	private static int lastHeight;
+
+	private static boolean continuousUpdates = false;
+	private static boolean dragged = false;
+	
+	final static JLabel imageLabel = new JLabel();
+	final static JPanel imagePanel = new JPanel();
 
 	public static void main(String[] args) {
 		
@@ -63,37 +86,63 @@ public class Main {
 		GridBagConstraints c = new GridBagConstraints();
 		frame.add(masterPanel);
 		
-		/*final JSplitPane imagePaneOuter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-		final JSplitPane imagePaneInner = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 		c.gridx = 0;
 		c.gridy = 0;
 		c.insets = new Insets(15, 15, 15, 15);
-		final JLabel imageLabel = new JLabel();
-		imageLabel.setPreferredSize(new Dimension(600, 400));
-		imageLabel.setBackground(new Color(200, 200, 200));
-		imageLabel.setBorder(BorderFactory.createCompoundBorder(
-				BorderFactory.createRaisedBevelBorder(),
-				BorderFactory.createLoweredBevelBorder()));
-		imagePaneInner.setTopComponent(imageLabel);
-		final JLabel emptyLabel = new JLabel();
-		imagePaneInner.setRightComponent(emptyLabel);
-		
-		imagePaneOuter.setTopComponent(imagePaneInner);
-		imagePaneOuter.setBottomComponent(emptyLabel);
-		masterPanel.add(imagePaneOuter, c);*/
-		
-		final JPanel imagePanel = new JPanel();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.insets = new Insets(15, 15, 15, 15);
-		final JLabel imageLabel = new JLabel();
-		imageLabel.setPreferredSize(new Dimension(600, 400));
-		imageLabel.setBackground(new Color(200, 200, 200));
+		imageLabel.setPreferredSize(new Dimension(MAX_DIMENSION.width, (int) (MAX_DIMENSION.getHeight())));
+		imageLabel.setBackground(new Color(220, 220, 220));
+		imageLabel.setOpaque(true);
 		imageLabel.setBorder(BorderFactory.createCompoundBorder( BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder()));
+		imageLabel.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if (dragged && bufferedImage != null) {
+					update();
+				}
+				dragged = false;
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		imageLabel.addMouseMotionListener(new MouseMotionListener(){
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if (continuousUpdates) {
+					update();
+				}
+				else {
+					dragged = true;
+				}
+			}
 
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				//System.out.println(e.getX() + ", " + e.getY());
+			}
+        	
+        });
 		ComponentResizer cr = new ComponentResizer();
 		cr.registerComponent(imageLabel);
-		cr.setMaximumSize(new Dimension(MAX_DIMENSION, MAX_DIMENSION));
+		cr.setMinimumSize(MIN_DIMENSION);
+		cr.setMaximumSize(MAX_DIMENSION);
+		cr.setSnapSize(new Dimension(1, 1));
+		cr.setDragInsets(new Insets(20, 20, 20, 20));
 		imagePanel.add(imageLabel);
 		masterPanel.add(imagePanel, c);
 		
@@ -116,20 +165,15 @@ public class Main {
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
 					File file = fileChooser.getSelectedFile();
 					
-					BufferedImage bufferedImage = null;
 					try {
 						bufferedImage = ImageIO.read(file);
-						int width = bufferedImage.getWidth();
-						int height = bufferedImage.getHeight();
-						int size = Math.max(width, height);
-						double scaleRatio = (double) MAX_DIMENSION / size;
-						int newWidth = (int) (width * scaleRatio);
-						int newHeight = (int) (height * scaleRatio);
-						Image image = bufferedImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-						imageLabel.setIcon(new ImageIcon(image));
-						imageLabel.setPreferredSize(new Dimension(newWidth, newHeight));
+						int[] displayData = displayImage(SobelFilter.filter(bufferedImage), true);
 						filenameLabel.setText(file.getName());
-						frame.pack();
+
+						originalWidth = bufferedImage.getWidth();
+						originalHeight = bufferedImage.getHeight();
+						scaledWidth = displayData[0];
+						scaledHeight = displayData[1];
 					}
 					catch (Exception e) {
 						e.printStackTrace();
@@ -154,6 +198,41 @@ public class Main {
 		frame.requestFocus();
 		frame.setVisible(true);
 		
+	}
+	
+	private static void update() {
+		int newWidth = imageLabel.getWidth();
+		int newHeight = imageLabel.getHeight();
+		if (lastWidth != newWidth || lastHeight != newHeight) {
+			System.out.println(imageLabel.getWidth() + " x " + imageLabel.getHeight());
+			System.out.println("Old image (Original: " + originalWidth + " x " + originalHeight + ", Scaled: "
+					+ scaledWidth + " x " + scaledHeight + ")");
+			double widthRatio = (double) newWidth / scaledWidth;
+			double heightRatio = (double) newHeight / scaledHeight;
+			System.out.println("New image (Original: " + (int) (originalWidth * widthRatio) + " x "
+					+ (int) (originalHeight * heightRatio) + ", Scaled: " + newWidth + " x " + newHeight + ")");
+			lastWidth = newWidth;
+			lastHeight = newHeight;
+			displayImage(SobelFilter.filter(bufferedImage), false);
+		}
+	}
+	
+	private static int[] displayImage(BufferedImage bufferedImage, boolean resize) {				
+		int width = bufferedImage.getWidth();
+		int height = bufferedImage.getHeight();
+		int size = Math.max(width, height);
+		double scaleRatio = Math.min(MAX_DIMENSION.width, MAX_DIMENSION.height) * INITIAL_SIZE_RATIO / size;
+		int newWidth = (int) (width * scaleRatio);
+		int newHeight = (int) (height * scaleRatio);
+		Image image = bufferedImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+		imageLabel.setIcon(new ImageIcon(image));
+		if (resize) {
+			imageLabel.setPreferredSize(new Dimension(newWidth, newHeight));
+			//imagePanel.setPreferredSize(new Dimension((int) (newWidth / INITIAL_SIZE_RATIO), (int) (newHeight / INITIAL_SIZE_RATIO)));
+			imagePanel.setPreferredSize(MAX_DIMENSION);	
+		}
+		
+		return new int[]{newWidth, newHeight};
 	}
 	
 }
